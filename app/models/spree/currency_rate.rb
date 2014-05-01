f2b6e32b8e9f3ad7b62eecc0dc477df4f5fc9751
kenleytomlin <kenleytomlin@gmail.com>
2014-05-01 06:18:36 +0100

    Making variable names more readable

--- app/models/spree/currency_rate.rb
+++ app/models/spree/currency_rate.rb
@@ -5,13 +5,12 @@ module Spree
   class CurrencyRate < ActiveRecord::Base
     validates :rate, :base_currency, :target_currency, presence: true
     validates :rate, numericality: true
-    monetize :rate
 
-    def convert_to_won(price)
-      price_usd = price.to_money("USD")
+    def convert_to_won(amount)
+      amount_in_usd = amount.to_money("USD")
       Money::Money.add_rate(self.base_currency,self.target_currency,self.rate)
-      price_in_won = Money::Money.us_dollar(price_usd).exchange_to('KRW')
-      price_in_won
+      amount_in_won = Money::Money.us_dollar(amount_in_usd).exchange_to('KRW')
+      amount_in_won
     end
 
   end
