2c8682bc893b1c6278ae82e42bf6495bf04c0b7c
kenleytomlin <kenleytomlin@gmail.com>
2014-05-22 13:57:51 +0100

    Made extension config names clearer

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,3 +1,4 @@
 Spree::AppConfiguration.class_eval do
-  preference :secondary_currency, :string, :default => 'KRW'
+  preference :settlement_currency, :string, :default => 'USD'
+  preference :presentation_currency, :string, :default => 'KRW'
 end
