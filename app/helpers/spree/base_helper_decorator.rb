79bd52e647709d9dab24074fa8aeef4058e96163
kenleytomlin <kenleytomlin@gmail.com>
2014-05-23 15:04:24 +0100

    Modified display_price to search for the price itself as it was unable to find the price using the passed in product.  Seems like a Spree bug.

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,6 +1,7 @@
 Spree::BaseHelper.module_eval do
   def display_price(product_or_variant,currency=nil)
     currency = if currency.nil? then Spree::Config[:presentation_currency] else currency end
-    product_or_variant.price_in(currency).display_price.to_html
+    price = product_or_variant.prices.select { |pr| pr.currency == currency }.first
+    price.display_price.to_html
   end
 end
