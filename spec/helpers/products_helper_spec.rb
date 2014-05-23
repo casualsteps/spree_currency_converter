14ca244d995d36fff7f633055d9d6906e0b9ce57
kenleytomlin <kenleytomlin@gmail.com>
2014-05-23 10:58:22 +0100

    variant_price green lights!

--- spec/helpers/products_helper_spec.rb
+++ spec/helpers/products_helper_spec.rb
@@ -6,25 +6,28 @@ module Spree
       reset_spree_preferences do |config|
         config.presentation_currency = "KRW"
         config.settlement_currency = "USD"
+        config.show_variant_full_price = true
       end
       Spree::CurrencyRate.create(:base_currency => "USD", :target_currency => "KRW", :rate => 1050)
     end
+
     let(:product) { create :multi_currency_product }
     let(:variant) { create :multi_currency_variant }
 
     context "variant_price" do
       describe "when the presentation_currency is KRW" do
         it "should return the KRW price" do
-          expect(variant_price(variant)).to eq("300000")
+          #result is encoded as a html string and the ₩ symbol is converted to
+          #this &#x20A9
+          expect(variant_price(variant)).to eq("&#x20A9;20,989")
         end
       end
 
       describe "when the presentation_currency is KRW but currency USD is passed" do
         it "should return the price in USD" do
-          expect(variant_price(variant)).to eq("200")
+          expect(variant_price(variant,'USD')).to eq("$200.00")
         end
       end
     end
-
   end
 end
