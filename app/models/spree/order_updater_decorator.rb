17015124a189af808331fb53db6cf65abac51380
James Duffy <jamesmartinduffy@gmail.com>
2014-06-11 18:38:25 +0900

    Added bugasae/gwansae update to persist_totals

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -42,6 +42,8 @@ Spree::OrderUpdater.class_eval do
       presentation_total: order.presentation_total,
       updated_at: Time.now,
     )
+    # running this will update bugasae and gwansae on the order record
+    Spree::Calculator::KoreanSurfaceMail.first.compute(order)
   end
 
   def update_presentation_total
