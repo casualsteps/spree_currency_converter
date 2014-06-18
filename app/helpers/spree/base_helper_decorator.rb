c5168b58e7bb7fca51053083cfbdf30d454ea80e
yjthegenius <yuzong@yujong-ui-MacBook-Pro.local>
2014-06-18 13:59:23 -0700

    display price range instead of one price

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,7 +1,14 @@
 Spree::BaseHelper.module_eval do
   def display_price(product_or_variant,currency=nil)
     currency = if currency.nil? then Spree::Config[:presentation_currency] else currency end
-    price = product_or_variant.prices.select { |pr| pr.currency == currency }.first
-    price.display_price.to_html
+    prices = product_or_variant.prices.select { |pr| pr.currency == currency }
+    max_price = prices.max_by(&:price)
+    min_price = prices.min_by(&:price)
+
+    if max_price.price == min_price.price
+      max_price.display_price.to_html
+    else
+      min_price.display_price.to_html + " ~ " + max_price.display_price.to_html
+    end
   end
 end
