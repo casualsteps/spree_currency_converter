802a77ed0d3b3f252e0ade3a2c24dbc819d9e3fd
James Duffy <jamesmartinduffy@gmail.com>
2014-07-21 00:12:11 +0900

    Trying to better handle items with no variants

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,12 +2,14 @@ Spree::BaseHelper.module_eval do
   def display_price(product_or_variant,currency=nil)
     currency = if currency.nil? then Spree::Config[:presentation_currency] else currency end
     prices = product_or_variant.prices.select { |pr| pr.currency == currency }
+    if prices.empty? && product_or_variant.master
+      prices = product_or_variant.master.prices.select { |pr| pr.currency == currency }
+    end
     max_price = prices.max_by(&:price)
     min_price = prices.min_by(&:price)
 
-    if !max_price || !min_price
-      # probably means there's no variants. Let's try returning the master price if it exists, otherwise there must be an error
-      return product_or_variant.master.display_price ? product_or_variant.master.display_price.to_html : "재고가 없습니다"
+    if prices.empty?
+      return "재고가 없습니다"
     elsif max_price.price == min_price.price
       max_price.display_price.to_html
     else
