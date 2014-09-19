f1d4d9f4392d7166c17edc46ebeb7defd479a045
JonghunYu <yuzong@naver.com>
2014-09-19 16:05:56 +0900

    revert lastest change

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,7 +1,7 @@
 Spree::BaseHelper.module_eval do
   def display_price(product_or_variant,currency=nil)
     currency = if currency.nil? then Spree::Config[:presentation_currency] else currency end
-    prices = product_or_variant.prices.select { |pr| pr.currency == currency && !pr.variant.deleted? }
+    prices = product_or_variant.prices.select { |pr| pr.currency == currency }
     if prices.empty? && product_or_variant.master
       prices = product_or_variant.master.prices.select { |pr| pr.currency == currency }
     end
