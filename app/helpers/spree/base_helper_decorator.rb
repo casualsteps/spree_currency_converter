de2f12e2c657e8b3cc1a93cc5c3d829c1dd227a9
James Martin Duffy <jamesmartinduffy@gmail.com>
2014-11-29 22:52:31 +0900

    Updated display_price method for performance boost

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,16 +1,10 @@
 Spree::BaseHelper.module_eval do
   def display_price(product_or_variant,currency=nil)
     currency = if currency.nil? then Spree::Config[:presentation_currency] else currency end
-    prices = product_or_variant.prices.select { |pr| pr.currency == currency && pr.variant.in_stock? }
-    if prices.empty? && product_or_variant.master
-      prices = product_or_variant.master.prices.select { |pr| pr.currency == currency }
-    end
-    max_price = prices.max_by(&:price)
-    min_price = prices.min_by(&:price)
+    min_price = product_or_variant.prices.where(currency: currency).order(amount: :asc).first
+    max_price = product_or_variant.prices.where(currency: currency).order(amount: :desc).first
 
-    if prices.empty?
-      return "재고가 없습니다"
-    elsif max_price.price == min_price.price
+    if max_price == min_price.price
       max_price.display_price.to_html
     else
       ### Temporary - naver only allows one price per product, so applying this patch
