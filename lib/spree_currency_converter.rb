211f59cd9be9730a8fcb32f31fd56b6d02b1feae
Lee Jun Hyuk <miriya.lee@gmail.com>
2016-05-02 12:30:36 +0900

    Update spree_currency_converter.rb

--- lib/spree_currency_converter.rb
+++ lib/spree_currency_converter.rb
@@ -48,7 +48,7 @@ class Money
       end
 
       def exchange_rates(date)
-        { 'USD' => BigDecimal('1,157.13') }
+        { 'USD' => BigDecimal('1157.13') }
         # page = RestClient.post(KR_EXCHANGE_RATE_URL,{ 'currDate' => date.strftime("%F"), 'eximDitc' => 2, 'x' => 38, 'y' => 10 })
         # Nokogiri::HTML(page).xpath('//*[@id="txt"]/div/table/tbody').css('tr').each_with_object({}) do |row, all|
         #   cols = row.css('td')
